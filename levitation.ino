c4eb0d24d6e51c1bdca43783e9c87166243d2270
Eric Schneider <franz.eric.schneider@gmail.com>
2015-04-12 10:35:31 -0400

    Untested lead compensation code

--- levitation.ino
+++ levitation.ino
@@ -2,12 +2,15 @@
 #define MID_PWM 192
 #define KP 10
 
-int sensorPin = A0;
-int sensorValue = 385;
+int sensor_pin = A0;
+int sensor_value = 385;
+int error = 0;
+int last_error = 0;  // Will be used for lead compensation
+int output = 0;
+int last_output = 0;  // Will be used for lead compensation
 
 void setup() {
-  Serial.begin(9600);
-  // put your setup code here, to run once:
+//  Serial.begin(9600);  // Uncomment for debugging
   //No prescaling on PWM clock
   TCCR0B |= _BV(CS00);
   TCCR0B &= ~(_BV(CS01) | _BV(CS02));
@@ -22,8 +25,14 @@ uint8_t counter = 0;
 uint8_t duty_cycle = 32;
 
 void loop() {
-  sensorValue = analogRead(sensorPin);
-  Serial.print("Sensor voltage: "); Serial.println(sensorValue);
+  sensorValue = analogRead(sensor_pin);
+  error = GOAL_VALUE - sensorValue;
+  // Serial.print("Sensor voltage: "); Serial.println(sensor_value);  // Uncomment for debugging
 
-  OCR0B = MID_PWM + ((GOAL_VALUE - sensorValue) * KP);
+  // Lead compensation for 50 rad/s hump at 0.001 sampling period
+  output = 9.341 * error - 9.194 * last_error + 0.8535 * last_output;
+  OCR0B = MID_PWM + output;
+
+  last_error = error;
+  last_output = output;
 }
